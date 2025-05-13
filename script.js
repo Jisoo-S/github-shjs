046afdd813341b3a3c02b452a0ab746eebff8651
Jisoo-S <shinjs0912@gmail.com>
2025-05-13 18:16:01 +0900

    5/13

--- script.js
+++ script.js
@@ -557,12 +557,7 @@ function handleCategoryChange() {
   document.getElementById("category-view").style.display = "block";
 }
 
-<<<<<<< HEAD
 // Firebase 초기화
-=======
-
- // Firebase 초기화
->>>>>>> e0778fabb1fdf86469a56e7a058d5c1ffcf5e276
 const firebaseConfig = {
   apiKey: "AIzaSyADIDRqmGCI6PGofskRtVnrsTK2xHpoqEw",
   authDomain: "logintodo-ff777.firebaseapp.com",
@@ -618,7 +613,6 @@ auth.onAuthStateChanged((user) => {
     userInfo.innerText = "로그인 안됨";
   }
 });
-<<<<<<< HEAD
 
 function showModal(message, withInput = false, callback) {
   const overlay = document.getElementById("modal-overlay");
@@ -660,5 +654,3 @@ function showModal(message, withInput = false, callback) {
 
 }
 
-=======
->>>>>>> e0778fabb1fdf86469a56e7a058d5c1ffcf5e276
